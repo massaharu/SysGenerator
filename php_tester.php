48024bc2be06a030eedd378560c06f61451079e0
massaharu <massaharu@impacta.com.br>
2015-10-26 18:55:18 -0200

    updates

--- php_tester.php
+++ php_tester.php
@@ -1,188 +1,74 @@
 <?php
-class Alun {
+Datefield
+{
+	xtype: 'datefield',
+	format:'d/m/Y',
+	fieldLabel: 'Data de Nascimento',
+	id:'idsolicitacaosecretariadtnascaluno',
+}
+Textfield
+{
+	xtype:"textfield",
+	width:127,
+	id:"idsolicitacaosecretariacelularaluno"
+}
+Numberfield
+{
+	xtype:'numberfield',
+	id:'idnbadmdescontomaior',
+	fieldLabel:'% Maior',
+	enableKeyEvents: true,
+	maxValue: 100,
+	listeners:{
+		'blur': function($this){
+			fn_enableDia({
+				nmfield: $this,
+				descontoitemtipo: 'mensalidade'
+			});
+		},
+		'keydown': function($this, e){
+			
+			if(e.getKey() == e.ENTER){
+				
+				fn_enableDia({
+					nmfield: $this,
+					descontoitemtipo: 'mensalidade'
+				});
+			}
+		}
+	}
+}
 
-    private $idalun;
-    private $desalun;
-    private $instatus;
-    private $dtcadastro;
+Checkbox
+,{
+	xtype:'compositefield',
+	hideLabel: true,
+	items:[{
+		xtype:'displayfield',
+		value:'Apresentar carta de desconto: '
+	},{
+		xtype:'checkbox',
+		id:'ckcartadesconto'
+	}]
+}
 
-    public function getIdalun(){
-        return $this->idalun;
-    }
-
-    public function setIdalun($idalun){
-        $this->idalun = $idalun;
-    }
-
-    public function getDesalun(){
-        return $this->desalun;
-    }
-
-    public function setDesalun($desalun){
-        $this->desalun = $desalun;
-    }
-
-    public function getInstatus(){
-        return $this->instatus;
-    }
-
-    public function setInstatus($instatus){
-        $this->instatus = $instatus;
-    }
-
-    public function getDtcadastro(){
-        return $this->dtcadastro;
-    }
-
-    public function setDtcadastro($dtcadastro){
-        $this->dtcadastro = $dtcadastro;
-    }
-
-    public function __construct($data = array()){
-
-        if(gettype($data) == "numeric" || gettype($data) == "integer" || gettype($data) == "string"){
-
-            return $this->load($data);
-
-        }else if(gettype($data) == "array") {
-
-            $this->setIdalun($data['idalun']);
-            $this->setDesalun($data['desalun']);
-            $this->setInstatus($data['instatus']);
-            $this->setDtcadastro($data['dtcadastro']);
-
-        }else{
-
-            return false;
-        }
-    }
-
-    public function load($idalun){
-
-        $select =
-        "SELECT 
-            idalun
-            ,desalun
-            ,instatus
-            ,dtcadastro
-        FROM tb_aluns
-        WHERE idalun = $idalun;";
-
-        $result = SQL::arrays($select, false);
-
-        $this->setIdalun($result['idalun']);
-        $this->setDesalun($result['desalun']);
-        $this->setInstatus($result['instatus']);
-        $this->setDtcadastro($result['dtcadastro']);
-
-        return $result;
-    }
-
-    public function get($idalun){
-
-        $select =
-        "SELECT 
-            idalun
-            ,desalun
-            ,instatus
-            ,dtcadastro
-        FROM tb_aluns
-        WHERE idalun = $idalun;";
-
-        $result = SQL::arrays($select, false);
-
-        return $result;
-    }
-
-    public function listAll(){
-
-        $select =
-        "SELECT 
-            idalun
-            ,desalun
-            ,instatus
-            ,dtcadastro
-        FROM tb_aluns
-        ORDER BY dtcadastro;";
-
-        $result = SQL::arrays($select);
-
-        return $result;
-    }
-
-    public function listAllActives(){
-
-        $select =
-        "SELECT 
-            idalun
-            ,desalun
-            ,instatus
-            ,dtcadastro
-        FROM tb_aluns
-        WHERE instatus = 1
-        ORDER BY dtcadastro;";
-
-        $result = SQL::arrays($select, false);
-
-        return $result;
-    }
-
-    public function save(){
-
-        $save = "";
-
-        if(count($this->get($this->getIdalun())) > 0){
-
-            $save = 
-            "UPDATE tb_aluns
-            SET
-                desalun = '".$this->getDesalun()."'
-                ,instatus = '".$this->getInstatus()."'
-            WHERE
-                idalun = $this->getIdalun();
-
-            SELECT idalun FROM tb_aluns WHERE idalun = $this->getIdalun();";
-
-        } else {
-
-            $save = 
-            "INSERT INTO tb_aluns(
-                desalun
-                ,instatus
-                ,dtcadastro
-            ) VALUES (
-                '".$this->getDesalun()."'
-                ,'".$this->getInstatus()."'
-                ,'".date('Y-m-d')."'
-            );
-
-            SELECT LAST_INSERT_ID() as idalun;";
-
-        }
-
-        $result = SQL::arrays($save, false);
-
-        return $result;
-    }
-
-    public function remove(){
-
-        $remove =
-        "UPDATE tb_aluns
-            instatus = 0
-        WHERE idalun = $this->getIdalun();";
-
-        SQL::query($remove);
-    }
-
-    public function del(){
-
-        $del =
-        "DELETE FROM tb_aluns
-        WHERE idalun = $this->getIdalun();";
-
-        SQL::query($del);
-    }
-
+{
+	xtype: 'combo',
+	fieldLabel:'Tipo de Desconto',
+	width:200,
+	valueField: 'iddescontotipo',
+	displayField: 'desdescontotipo',
+	typeAhead: true,
+	mode: 'local',
+	triggerAction: 'all',
+	emptyText: 'Selecione o tipo de desconto.',
+	selectOnFocus: true,
+	id: 'cmbadmdescontotipodesc',
+	allowBlank: false,
+	store:storelist_descontoTipoList,
+	listeners:{
+		'select':function(combo, record, index){
+		}
+	}
 }
 ?>
